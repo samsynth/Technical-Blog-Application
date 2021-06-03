d9dce9c7f4ec20a6e9dd62d42b777ee718dcd36c
samsynth <samarthmoh@gmail.com>
2021-06-03 12:15:38 +0530

    fifth version

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import technicalblog.model.Post;
 import technicalblog.model.User;
+import technicalblog.model.UserProfile;
 import technicalblog.service.PostService;
 import technicalblog.service.UserService;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,14 +25,24 @@ public class UserController {
     public String login(){
         return "users/login";
     }
+
     @RequestMapping("users/registration")
-    public String registration(){
+    public String registration(Model model){
+        User user = new User();
+        UserProfile profile= new UserProfile();
+        user.setProfile(profile);
+
+        model.addAttribute("User",user);
+
         return "users/registration";
     }
 
     @RequestMapping(value = "users/login", method=RequestMethod.POST)
-    public String loginUser(User user) {
-        if(userService.login(user)) {
+    public String loginUser(User user, HttpSession session) {
+        User existingUser = userService.login(user);
+
+        if(existingUser!=null) {
+            session.setAttribute("loggeduser", existingUser);
             return "redirect:/posts";
         }
         else {
@@ -40,11 +52,13 @@ public class UserController {
 
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
     public String registerUser(User user){
+        userService.registerUser(user);
         return "users/login";
     }
 
     @RequestMapping(value="users/logout", method = RequestMethod.POST)
-    public String logout(Model model){
+    public String logout(Model model, HttpSession session){
+        session.invalidate();
         List<Post> posts = postService.getAllPosts();
         model.addAttribute("posts", posts);
     return "index";
--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -1,5 +1,7 @@
 package technicalblog.model;
 
+import javafx.geometry.Pos;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -23,6 +25,18 @@ public class Post {
     @Column(name = "date")
     private Date date;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Category> categories= new ArrayList<>();
+
+    @Transient
+    private String springBlog;
+
+    @Transient
+    private String javaBlog;
 
     public Integer getId() {
         return id;
@@ -54,5 +68,33 @@ public class Post {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public User getUser() {   return user;   }
+
+    public void setUser(User user) { this.user = user;    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getSpringBlog() {
+        return springBlog;
+    }
+
+    public void setSpringBlog(String springBlog) {
+        this.springBlog = springBlog;
+    }
+
+    public String getJavaBlog() {
+        return javaBlog;
+    }
+
+    public void setJavaBlog(String javaBlog) {
+        this.javaBlog = javaBlog;
     }
 }
